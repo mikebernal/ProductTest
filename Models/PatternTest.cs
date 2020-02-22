e35c3a21b7a2cb335f3f71336b54ff9cc104948f
mikebernal <mikebernal.me@gmail.com>
2020-02-22 18:21:05 +0800

    Revert db table fields data types

--- Models/PatternTest.cs
+++ Models/PatternTest.cs
@@ -9,23 +9,23 @@ namespace ProductTest.Models
         public string SalesHook { get; set; }
         public string Features { get; set; }
         public string Images { get; set; }
-        public bool Active { get; set; }
+        public string Active { get; set; }
         public string Synonyms { get; set; }
-        public bool Performance { get; set; }
-        public bool LowerNoise { get; set; }
-        public bool HigherMilage { get; set; }
-        public bool UltraHighPerformance { get; set; }
-        public bool Eco { get; set; }
-        public bool SemiSlick { get; set; }
-        public bool Touring { get; set; }
-        public bool AllSeason { get; set; }
-        public bool HighwayTerrain { get; set; }
-        public bool AllTerrain { get; set; }
-        public bool HeavyAllTerrain { get; set; }
-        public bool MudTerrain { get; set; }
+        public string Performance { get; set; }
+        public string LowerNoise { get; set; }
+        public string HigherMilage { get; set; }
+        public string UltraHighPerformance { get; set; }
+        public string Eco { get; set; }
+        public string SemiSlick { get; set; }
+        public string Touring { get; set; }
+        public string AllSeason { get; set; }
+        public string HighwayTerrain { get; set; }
+        public string AllTerrain { get; set; }
+        public string HeavyAllTerrain { get; set; }
+        public string MudTerrain { get; set; }
         public string TreadType { get; set; }
         public string QualityCategory { get; set; }
-        public bool ColourSmoke { get; set; }
+        public string ColourSmoke { get; set; }
         public string Segment { get; set; }
         public string Key { get; set; }
     }
