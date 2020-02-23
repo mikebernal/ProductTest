183b41e1f2d418a60d6122a92afd7cf8abb94860
mikebernal <mikebernal.me@gmail.com>
2020-02-23 13:49:29 +0800

    Fixed sku table

--- Models/SkuTest.cs
+++ Models/SkuTest.cs
@@ -1,27 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProductTest.Models
 {
     public class SkuTest
     {
         public int Id { get; set; }
+        [Required]
         public string BrandTitle { get; set; }
+        [Required]
         public string PatternTitle { get; set; }
+        [Required]
         public string Sku { get; set; }
-        public int Width { get; set; }
-        public int AspectRatio { get; set; }
-        public int RimSize { get; set; }
-        public int LoadRating1 { get; set; }
-        public int LoadRating2 { get; set; }
+        public int? Width { get; set; }
+        public int? AspectRatio { get; set; }
+        public int? RimSize { get; set; }
+        public int? LoadRating1 { get; set; }
+        public int? LoadRating2 { get; set; }
         public string Speed { get; set; }
         public string Description { get; set; }
-        public bool Active { get; set; }
+        public bool? Active { get; set; }
         public string Homologation1 { get; set; }
         public string Homologation2 { get; set; }
-        public bool RunFlat { get; set; }
-        public bool MOE { get; set; }
-        public bool SUV { get; set; }
-        public bool Seal { get; set; }
-        public bool NoiseCancelling { get; set; }
-        public bool LightTruck { get; set; }
-        public bool Commercial { get; set; }
+        public bool? RunFlat { get; set; }
+        public bool? MOE { get; set; }
+        public bool? SUV { get; set; }
+        public bool? Seal { get; set; }
+        public bool? NoiseCancelling { get; set; }
+        public bool? LightTruck { get; set; }
+        public bool? Commercial { get; set; }
     }
 }
